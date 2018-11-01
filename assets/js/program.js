2f3027383ccd109743223eebf411c70e1f41068f
Andrew <8879789+manleyac@users.noreply.github.com>
2018-11-01 18:20:28 -0400

    updating time calc

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,11 +1,19 @@
 
 function addTrain(event) {
    event.preventDefault();
+
+   let time = $("input[name=firstDeparture]").val(); 
+   let hour = parseInt(time.split(":")[0]);
+   let min = parseInt(time.split(":")[1]);
+   
+   let leave = moment().hour(hour).minute(min);
+   console.log(leave);
+
    let newTrain = $("<tr>");
 
    let name = $("<td>").text($("input[name=trainName]").val());
    let dest = $("<td>").text($("input[name=destination]").val());
-   let first = $("<td>").text($("input[name=firstDeparture]").val());
+   let first = $("<td>").text(leave.format("hh:mm"));
    let repeat = $("<td>").text($("input[name=frequency]").val());
 
    newTrain.append(name, dest, first, repeat);
